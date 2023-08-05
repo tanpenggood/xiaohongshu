337ffb135b4cbe466d09bc950eeedcb6c9d9d9b3
tanpenggood <tanpengswpu@163.com>
2023-08-05 14:45:35 +0800

    optimize(XhsCrawlab.java): XhsCrawlab use single pattern

--- src/main/java/com/itplh/xhs/XhsCrawlab.java
+++ src/main/java/com/itplh/xhs/XhsCrawlab.java
@@ -18,7 +18,49 @@ import java.util.concurrent.TimeUnit;
  */
 public class XhsCrawlab {
 
-    public static UserInfo crawlHome(String homeUrl) {
+    private volatile static XhsCrawlab instance;
+
+    private DelayVariable delay1Second = new DelayVariable(1, TimeUnit.SECONDS);
+
+    private Map<String, String> headers = new HashMap();
+
+    {
+        headers.put("cookie", "a1=189740d9141vtgdxvnnlyxs9snsf3rmeeftx96nv230000320550; webId=1f9f30cf48f276899f57fccc10a18596; gid=yYjW48fjWKd2yYjW48fjyiKI4yh9kfChII1v9hJCMjMIMCq8iqF6dj888qJ82288YWjDJ8Wj; cache_feeds=[]; web_session=040069b23c59f037b9a3da138c364b0dee0d1a; xsecappid=xhs-pc-web; webBuild=2.17.8; websectiga=984412fef754c018e472127b8effd174be8a5d51061c991aadd200c69a2801d6; sec_poison_id=4e6e1799-dc39-49b5-bb3a-241a1fe8dc26");
+        // PC
+        this.headers.put("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36");
+        // Android
+//        headers.put("User-Agent", "Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko)");
+    }
+
+    private XhsCrawlab() {
+        // 私有构造方法，防止外部实例化
+    }
+
+    public static XhsCrawlab getInstance() {
+        if (instance == null) {
+            synchronized (XhsCrawlab.class) {
+                if (instance == null) {
+                    instance = new XhsCrawlab();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public XhsCrawlab setHeaders(Map<String, String> headers) {
+        if (CollectionUtils.isEmpty(headers)) {
+            return this;
+        }
+        this.headers.putAll(headers);
+        return this;
+    }
+
+    public XhsCrawlab setCookie(String cookie) {
+        this.headers.put("cookie", cookie);
+        return this;
+    }
+
+    public UserInfo crawlHome(String homeUrl) {
         System.out.println(homeUrl);
         Document document = HttpUtils.requestGet(homeUrl, headers, delay1Second).get();
 
@@ -42,29 +84,10 @@ public class XhsCrawlab {
         return userInfo;
     }
 
-    public static void crawlDetail(String detailUrl) {
+    public void crawlDetail(String detailUrl) {
         Document document = HttpUtils.requestGet(detailUrl, headers, delay1Second).get();
         // parse note data - json
         JsonParserSelector.parseDetailPageJson(document);
-    }
-
-    public static void setHeaders(Map<String, String> headers) {
-        if (CollectionUtils.isEmpty(headers)) {
-            return;
-        }
-        headers.putAll(headers);
-    }
-
-    private static DelayVariable delay1Second = new DelayVariable(1, TimeUnit.SECONDS);
-
-    private static Map<String, String> headers = new HashMap();
-
-    static {
-        headers.put("cookie", "a1=189740d9141vtgdxvnnlyxs9snsf3rmeeftx96nv230000320550; webId=1f9f30cf48f276899f57fccc10a18596; gid=yYjW48fjWKd2yYjW48fjyiKI4yh9kfChII1v9hJCMjMIMCq8iqF6dj888qJ82288YWjDJ8Wj; cache_feeds=[]; web_session=040069b23c59f037b9a3da138c364b0dee0d1a; xsecappid=xhs-pc-web; webBuild=2.17.8; websectiga=984412fef754c018e472127b8effd174be8a5d51061c991aadd200c69a2801d6; sec_poison_id=4e6e1799-dc39-49b5-bb3a-241a1fe8dc26");
-        // PC
-        headers.put("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36");
-        // Android
-//        headers.put("User-Agent", "Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko)");
     }
 
 }
